b361639ba2ac6c56b461ebdca1f8662e705f51c6
coa00 <coa@rokuro.net>
2020-04-22 09:52:48 +0900

    add nonce to template

--- html/wp-content/themes/twentytwenty/functions.php
+++ html/wp-content/themes/twentytwenty/functions.php
@@ -755,3 +755,42 @@ function twentytwenty_get_elements_array() {
 	*/
 	return apply_filters( 'twentytwenty_get_elements_array', $elements );
 }
+
+function custom_preview_page_link( $link ) {
+	$id = get_the_ID();
+	// For authentication a nonce must be created
+	// otherwise the REST API does not allow users to see previews
+	$nonce = wp_create_nonce( 'wp_rest' );
+	$post = get_post( $id );
+	$slug = $post->post_name;
+	$lang = '';
+	$isPost = '';
+
+	// if we preview a post, we want to add 'posts/' to the url to link to our posts site in the frontend
+	// TODO this URL schould be dynamicly the same for back- and frontend
+	if ($post->post_type === 'post') {
+		$isPost = '';
+	}
+
+	// if we have a new page without a slug we show /preview/ as a page
+	if ($slug === '') {
+		$slug = 'preview';
+	}
+
+	// Get the language of the polylang custom field
+	// https://polylang.wordpress.com/documentation/documentation-for-developers/functions-reference/
+	$current_lang = pll_get_post_language( $id );
+	$default_lang = pll_default_language( 'slug' );
+
+	// Only add a language if it's not the default language
+	if ($current_lang !== $default_lang) {
+		$lang = $current_lang . '/';
+	}
+
+	/* make sure WP_HOME (wp-config.php) always has exactly one trailing slash.
+	Remove if it has multiple then add one or add one if it has no slash */
+	$home_path = rtrim(WP_HOME, '/') . '/';
+	$link = $home_path . $lang . $isPost . $slug . '/?preview_id='. $id. '&preview_nonce=' . $nonce . '&preview=true';
+	return $link;
+}
+add_filter('preview_post_link', 'custom_preview_page_link');
